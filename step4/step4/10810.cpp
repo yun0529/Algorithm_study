d02ccbbe9fb380380bf21637e06fc9291aef87e0
yun0529 <0529yun@naver.com>
2024-02-06 22:34:00 +0900

    10810 #1

---
+++ step4/step4/10810.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int n, m = 0;
+//	int i, j, k;
+//
+//	cin >> n >> m;
+//
+//	int arr[100] = {0, };
+//	for (int r = 0; r < m; r++) {
+//		cin >> i >> j >> k;
+//
+//		for (int l = i-1; l < j; l++) {
+//			arr[l] = k;
+//		}
+//
+//	}
+//
+//	for (int r = 0; r < n; r++) {
+//		cout << arr[r] << " ";
+//	}
+//
+//	return 0;
+//}
