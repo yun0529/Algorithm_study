7f2e3768afe8de27b7f4805910e8446db8545bc2
yun0529 <0529yun@naver.com>
2024-02-09 21:08:05 +0900

    step5, step6 solving... #1

---
+++ step6/step6/2444.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int n;
+//	cin >> n;
+//
+//	for (int i = 0; i < n; i++) {
+//		for (int k = n; k > i+1; k--) {
+//			cout << " ";
+//		}
+//		for (int j = 0; j < 2 * i + 1; j++) {
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//	for (int i = 1; i < n; i++) {
+//		for (int k = 0; k < i; k++) {
+//			cout << " ";
+//		}
+//		for (int j = 2 * n; j > 2*i+1; j--) {
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//
+//	return 0;
+//}
---
+++ step6/step6/25083.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void) {
+//
+//	string s = "         ,r'\"7\n";
+//	s += "r`-_   ,'  ,/\n";
+//	s += " \\. \". L_r'\n";
+//	s += "   `~\\/\n";
+//	s += "      |\n";
+//	s += "      |";
+//	cout << s;
+//
+//	return 0;
+//}
