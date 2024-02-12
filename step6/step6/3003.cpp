d9728000bc7c491db39664a9588b5cb71afc0938
yun0529 <0529yun@naver.com>
2024-02-12 21:34:11 +0900

    10988 #1

--- step6/step6/3003.cpp
+++ step6/step6/3003.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-
-	int arr[6];
-
-	for (int i = 0; i < 6; i++) {
-		cin >> arr[i];
-	}
-	for (int i = 0; i < 6; i++) {
-		switch (i) {
-		case 0:
-			cout << 1 - arr[i]<< " ";
-			break;
-		case 1:
-			cout << 1 - arr[i] << " ";
-			break;
-		case 2:
-			cout << 2 - arr[i] << " ";
-			break;
-		case 3:
-			cout << 2 - arr[i] << " ";
-			break;
-		case 4:
-			cout << 2 - arr[i] << " ";
-			break;
-		case 5:
-			cout << 8 - arr[i] << " ";
-			break;
-		}
-
-	}
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int arr[6];
+//
+//	for (int i = 0; i < 6; i++) {
+//		cin >> arr[i];
+//	}
+//	for (int i = 0; i < 6; i++) {
+//		switch (i) {
+//		case 0:
+//			cout << 1 - arr[i]<< " ";
+//			break;
+//		case 1:
+//			cout << 1 - arr[i] << " ";
+//			break;
+//		case 2:
+//			cout << 2 - arr[i] << " ";
+//			break;
+//		case 3:
+//			cout << 2 - arr[i] << " ";
+//			break;
+//		case 4:
+//			cout << 2 - arr[i] << " ";
+//			break;
+//		case 5:
+//			cout << 8 - arr[i] << " ";
+//			break;
+//		}
+//
+//	}
+//
+//	return 0;
+//}
