6ee99c150db43fa9ad448445cc54cb1033c0386d
yun0529 <0529yun@naver.com>
2024-02-17 23:41:04 +0900

    2566 #1

---
+++ step7/step7/2566.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int arr[10][10];
+//	int max = -1;
+//	int col, raw;
+//	for (int i = 0; i < 9; i++) {
+//		for (int j = 0; j < 9; j++) {
+//			cin >> arr[i][j];
+//			if (arr[i][j] > max) {
+//				max = arr[i][j];
+//				col = i+1;
+//				raw = j+1;
+//			}
+//		}
+//	}
+//	cout << max << endl;
+//	cout << col << " " << raw;
+//
+//	return 0;
+//}
