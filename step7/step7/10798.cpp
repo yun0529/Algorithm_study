17ced05f106601b308aec6329c00b5f61e370e9b
yun0529 <0529yun@naver.com>
2024-02-18 23:33:34 +0900

    10798 #1

---
+++ step7/step7/10798.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+	string str[5];
+
+	for (int i = 0; i < 5; i++) {
+		cin >> str[i];
+	}
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (str[j].length() > i) {
+				cout << str[j][i];
+			}
+		}
+	}
+
+	return 0;
+}
