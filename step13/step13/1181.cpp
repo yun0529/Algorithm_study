16ee9aedd25d0efc20fefb2beee4527b6003da07
yun0529 <0529yun@naver.com>
2024-03-11 23:11:27 +0900

    1181 #1

---
+++ step13/step13/1181.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//
+//int cmp(string a, string b) {
+//	if (a.length() == b.length()) {
+//		return a < b;
+//	} 
+//	else {
+//		return a.length() < b.length();
+//	}
+//}
+//
+//
+//
+//int main() {
+//
+//	int n;
+//
+//	string word[20000];
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		cin >> word[i];
+//	}
+//
+//	sort(word, word + n, cmp);
+//
+//	for (int i = 0; i < n; i++) {
+//
+//		if (word[i] == word[i - 1]) {
+//			continue;
+//		}
+//		cout << word[i] << "\n";
+//	}
+//
+//	return 0;
+//}
