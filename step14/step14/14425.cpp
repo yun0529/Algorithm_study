7da40c8109244423f45b45194bfa01062ee26395
yun0529 <0529yun@naver.com>
2024-03-13 19:10:04 +0900

    step14... #1

---
+++ step14/step14/14425.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <vector>
+//#include <set>
+//#include <algorithm>
+//using namespace std;
+//
+//int main(void) {
+//
+//	cin.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//
+//	int n,m, count = 0;
+//	set<string> s;
+//
+//	cin >> n >> m;
+//	for (int i = 0; i < n; i++) {
+//		string word;
+//		cin >> word;
+//		s.insert(word);
+//	}
+//	for (int i = 0; i < m; i++) {
+//		string f;
+//		cin >> f;
+//		if (s.find(f) != s.end()) {
+//			count++;
+//		}
+//	}
+//	cout << count;
+//	return 0;
+//}
