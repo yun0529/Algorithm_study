92caa592b1afaf038c4a4446f9f335363017f6e1
yun0529 <0529yun@naver.com>
2024-03-14 18:03:36 +0900

    step14... #1

---
+++ step14/step14/10816.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include <vector>
+//#include <unordered_map>
+//#include <algorithm>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int n, m;
+//	cin >> n;
+//	vector<int> mine;
+//	vector<int> b;
+//	int a;
+//	for (int i = 0; i < n; i++) {
+//		cin >> a;
+//		mine.push_back(a);
+//	}
+//	cin >> m;
+//	unordered_map<int, int> num;
+//	for (int i = 0; i < m; i++) {
+//		cin >> a;
+//		b.push_back(a);
+//	}
+//	for (int i = 0; i < m; i++) {
+//		cout << count(mine.begin(), mine.end(), b[i]) << " ";
+//	}
+//	return 0;
+//}
