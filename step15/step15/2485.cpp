dbbbe8824ea892d7ee44343e4a0ec5c9d1bf191a
yun0529 <0529yun@naver.com>
2024-03-21 22:41:07 +0900

    2485 ... #1

--- step15/step15/2485.cpp
+++ step15/step15/2485.cpp
@@ -1,27 +1,44 @@
 //#include <iostream>
 //#include <vector>
+//#include <algorithm>
 //using namespace std;
+//
+//int max_num(int m, int dist) {
+//	int i = 1;
+//
+//	for (i = dist; i >= 1; i--) {
+//		if (m % dist == 0) {
+//			return i;
+//		}
+//	}
+//	cout << i << "\n";
+//	return i;
+//}
 //
 //int main(void) {
 //	cin.tie(NULL);
 //	ios_base::sync_with_stdio;
 //
-//	vector<int> a;
+//	vector<int> arr;
+//	vector<int> v;
 //	int n, x;
 //	cin >> n;
+//	int a = 0, b = 0;
 //	for (int i = 0; i < n; i++) {
-//		cin >> x;
-//		a.push_back(x);
+//		cin >> a;
+//		v.push_back(a);
 //	}
-//
-//	int count = 0;
-//	int diff = a[1] - a[0] ;
-//
-//	for (int i = 1; i < n; i++) {
-//		if (a[i] - a[i - 1] == diff) {
-//
-//		}
+//	for (int i = 0; i < n - 1; i++) {
+//		arr.push_back(v[i+1] - v[i]);
 //	}
+//	//sort(arr.begin(), arr.end());
+//	x = arr[0];
+//	for (int i = 1; i < arr.size(); i++) {
+//		x = max_num(x, arr[i]);
+//	}
+//	int diff = v[n-1] - v[0];
+//	
+//	cout << x << "\n" << (diff / x) - (n - 1);
 //
 //	return 0;
 //}
