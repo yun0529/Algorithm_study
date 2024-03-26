374e26cc6475db8871051e27f4b3de2aab76913a
yun0529 <0529yun@naver.com>
2024-03-26 14:28:26 +0900

    4134... #1

---
+++ step15/step15/4134.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main(void) {
+//
+//	long n, a;
+//	cin >> n;
+//
+//	vector<long> arr;
+//	for (long i = 0; i < n; i++) {
+//		cin >> a;
+//		arr.push_back(a);
+//	}
+//
+//	for (long i = 0; i < n; i++) {
+//		for (long i = arr[i]; i < arr[i] + 100000; i++) {
+//
+//		}
+//	}
+//
+//	return 0;
+//}
