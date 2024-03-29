0504dd523ba829ed599a281a5bd9c026c10ca681
yun0529 <0529yun@naver.com>
2024-03-29 21:50:14 +0900

    11866 #1

--- step16/step16/11866.cpp
+++ step16/step16/11866.cpp
@@ -6,9 +6,41 @@
 //	cin.tie(NULL);
 //	ios_base::sync_with_stdio(false);
 //
-//	int n, x;
-//	cin >> n;
-//	queue<int> qu;
+//	int n, k;
+//	cin >> n >> k;
+//	queue<int> in;
+//	queue<int> prn;
+//
+//	for (int i = 1; i <= n; i++) {
+//		in.push(i);
+//	}
+//	int count = 1;
+//	cout << "<";
+//	while (n > 0) {
+//		if (count % k == 0) {
+//			prn.push(in.front());
+//			in.pop();
+//			n--;
+//		}
+//		else {
+//			in.push(in.front());
+//			in.pop();
+//		}
+//		count++;
+//		
+//	}
+//
+//	int size = prn.size();
+//	for (int i = 0; i < size; i++) {
+//		cout << prn.front();
+//		prn.pop();
+//		if (prn.empty()) {
+//			cout << ">";
+//		}
+//		else {
+//			cout << ", ";
+//		}
+//	}
 //
 //	return 0;
 //}
--- step16/step16/2164.cpp
+++ step16/step16/2164.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-#include <queue>
-using namespace std;
-
-int main(void) {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
-	int n, x;
-	cin >> n;
-	queue<int> qu;
-
-	for (int i = 1; i <= n; i++) {
-		qu.push(i);
-	}
-	for (int i = 1; i <= n; i++) {
-		if (qu.size() == 1) {
-			cout << qu.front();
-			break;
-		}
-		qu.pop();
-		x = qu.front();
-		qu.pop();
-		qu.push(x);
-	}
-
-	return 0;
-}
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//int main(void) {
+//	cin.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//
+//	int n, x;
+//	cin >> n;
+//	queue<int> qu;
+//
+//	for (int i = 1; i <= n; i++) {
+//		qu.push(i);
+//	}
+//	for (int i = 1; i <= n; i++) {
+//		if (qu.size() == 1) {
+//			cout << qu.front();
+//			break;
+//		}
+//		qu.pop();
+//		x = qu.front();
+//		qu.pop();
+//		qu.push(x);
+//	}
+//
+//	return 0;
+//}
