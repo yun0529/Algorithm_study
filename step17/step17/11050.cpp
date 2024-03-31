d9bbf4e947bc018058477844d822fbae410733f7
yun0529 <0529yun@naver.com>
2024-03-31 18:21:06 +0900

    11050... #1

---
+++ step17/step17/11050.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int n, k, sum = 0;
+//	cin >> n >> k;
+//
+//	for (int i = n; i > 0; i--) {
+//		sum *= i/(i-k);
+//	}
+//	cout << sum;
+//	return 0;
+//}
---
+++ step17/step17/24723.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int n;
+//	cin >> n;
+//	//int sum = n * 2;
+//	//for (int i = 2; i <= n; i++) {
+//	//	sum += (i - 2)*2;
+//	//}
+//	cout << pow(2,n);
+//	return 0;
+//}
