b44cf3ab171e814d81cf8b4cb75c10070f0f69d8
yun0529 <0529yun@naver.com>
2024-04-02 01:23:37 +0900

    step17 #1

--- step17/step17/1010.cpp
+++ step17/step17/1010.cpp
@@ -1,19 +1,37 @@
-#include <iostream>
-using namespace std;
-
-int pec(int n) {
-	int sum = 1;
-	for (int i = n; i > 0; i--) {
-		sum *= i;
-	}
-	return sum;
-}
-
-int main(void) {
-
-	int n, k;
-	cin >> n >> k;
-
-	cout << pec(n) / pec(n - k) / pec(k);
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//long long comb(int n, int m) {
+//	int sum = 1;
+//	int bun = 1;
+//	for (int i = m; i > m-n; i--) {
+//		sum *= i;
+//		sum /= bun;
+//		bun++;
+//	}
+//	return sum;
+//}
+//
+//int main(void) {
+//
+//	int x, n, m, sum = 1;
+//	cin >> x;
+//	
+//	for (int k = 0; k < x; k++) {
+//		cin >> n >> m;
+//		/*sum = 1;
+//		if (n == 1) {
+//			cout << m << "\n";
+//			continue;
+//		}
+//		else {
+//			for (int i = 1; i <= (m - n); i++) {
+//				sum += (i+1) * (n - 1);
+//			}
+//			cout << sum << "\n";
+//		}*/
+//		cout << comb(n, m)<< "\n";
+//	}
+//	
+//	return 0;
+//}
