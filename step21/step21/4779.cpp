12f850a3beea2ba4b9addbdd900d505ec8133dc9
yun0529 <0529yun@naver.com>
2024-04-05 19:44:03 +0900

    4779... #1

---
+++ step21/step21/4779.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+//string str;
+//
+//string regStr(int n) {
+//	int k = 0;
+//
+//	if (n > 1) {
+//		while (k < n) {
+//			str.replace(n, 1, " ");
+//			k++;
+//		}
+//	}
+//
+//	return regStr(sqrt(n));
+//}
+//
+//int main(void) {
+//
+//	cin.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//
+//	int n;
+//	while (true) {
+//		str = "";
+//		cin >> n;
+//		for (int i = 0; i < pow(3,n); i++) {
+//			str.append("-");
+//		}
+//		regStr(n);
+//		cout << str << "\n";
+//	}
+//
+//	return 0;
+//}
