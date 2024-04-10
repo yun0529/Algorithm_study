c15d9b35bbd725818365627129339ab25e387bcc
yun0529 <0529yun@naver.com>
2024-04-10 22:31:06 +0900

    4779 #1

--- step21/step21/4779.cpp
+++ step21/step21/4779.cpp
@@ -2,19 +2,18 @@
 //#include <cmath>
 //using namespace std;
 //
-//string str;
+//void regStr(int n) {
+//	int k = pow(3,n-1);
 //
-//string regStr(int n) {
-//	int k = 0;
-//
-//	if (n > 1) {
-//		while (k < n) {
-//			str.replace(n, 1, " ");
-//			k++;
-//		}
+//	if (n == 0) {
+//		cout << "-";
+//		return;
 //	}
-//
-//	return regStr(sqrt(n));
+//	regStr(n - 1);
+//	for (int i = 0; i < k; i++) {
+//		cout << " ";
+//	}
+//	regStr(n - 1);
 //}
 //
 //int main(void) {
@@ -23,14 +22,10 @@
 //	ios_base::sync_with_stdio(false);
 //
 //	int n;
-//	while (true) {
-//		str = "";
-//		cin >> n;
-//		for (int i = 0; i < pow(3,n); i++) {
-//			str.append("-");
-//		}
+//	while (cin >> n) {
+//		
 //		regStr(n);
-//		cout << str << "\n";
+//		cout << "\n";
 //	}
 //
 //	return 0;
