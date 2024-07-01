f5972f4d30d8df6710ad02d5d9fdc48630bf2ddf
yun0529 <0529yun@naver.com>
2024-07-01 13:07:10 +0900

    week6 solving... #7

---
+++ lecture/ch6/ch6/BFS.cpp
@@ -0,0 +1,27 @@
+// BFS
+// 
+// 방문한 정점을 다시 방문하지 않는다.
+// 레밸별로 탐색한다.
+// qeueu를 사용
+// queue에 push하고 front와 연결된 것들을 push하고 가장 앞의 노드를 pop함.
+// 최단거리를 담는 것으로 사용할 수 있음.
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//void bfs(int here) {
+//	queue<int> q;
+//	q.push(here);
+//	visited[here] = 1;
+//	q.push(here);
+//	while (q.size()) {
+//
+//	}
+//}
+//
+//int main() {
+//
+//
+//
+//	return 0;
+//}
