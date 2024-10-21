8394a7002fb2aa53f395baf2ece0677818259082
yun0529 <0529yun@naver.com>
2024-10-21 14:35:54 +0900

    2792 solvin... #7

---
+++ lecture/ch10/ch10/2792.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[300004];
+
+int main(void) {
+
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> arr[i];
+	}
+
+	return 0;
+}
