3a92b0bdcac367ae05db562a558750fb52d910af
yun0529 <0529yun@naver.com>
2024-10-29 02:10:25 +0900

    1072 solve #7

--- lecture/ch10/ch10/1072.cpp
+++ lecture/ch10/ch10/1072.cpp
@@ -1,7 +1,9 @@
 //#include <iostream>
 //using namespace std;
 //
-//int x, y, z, ret = 0;
+//typedef long long ll;
+//
+//ll x, y, z, ret = 0;
 //
 //int main() {
 //
@@ -11,20 +13,19 @@
 //	cin >> x >> y;
 //	z = y * 100 / x;
 //
-//	int lo = y, hi = x;
+//	ll lo = 0, hi = 1000000000;
 //	while (lo <= hi) {
-//		int mid = (lo + hi) / 2;
-//		if (mid * 100 / x > z) {
+//		ll mid = (lo + hi) / 2;
+//		if ((mid + y) * 100 / (mid + x) > z) {
 //			hi = mid - 1;
 //			ret = mid;
 //		}
 //		else {
 //			lo = mid + 1;
-//
 //		}
 //	}
 //
-//	if (ret) cout << ret - y;
+//	if (ret) cout << ret;
 //	else cout << -1;
 //
 //	return 0;
