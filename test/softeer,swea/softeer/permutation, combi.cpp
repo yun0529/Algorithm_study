bcf677f9b1118ea46225a1452736e1eb2d585928
yun0529 <0529yun@naver.com>
2024-11-15 16:26:46 +0900

    swea10993 #7

--- test/softeer,swea/softeer/permutation, combi.cpp
+++ test/softeer,swea/softeer/permutation, combi.cpp
@@ -19,6 +19,39 @@
 ////	}
 ////}
 //
+//int n = 0, k = 0, cnt = 0;
+//void combi(int start, vector<int> b) {
+//	if (b.size() == k) {
+//		cnt++;
+//		return;
+//	}
+//	for (int i = start + 1; i < n; i++) {
+//		b.push_back(i);
+//		combi(i, b);
+//		b.pop_back();
+//	}
+//}
+//
+//long long ret;
+//long long pec(int n) {
+//	long long sum = 1;
+//	for (int i = 1; i <= n; i++) {
+//		sum *= i;
+//	}
+//	return sum;
+//}
+//
+//int pascal[1004][1004];
+//void go() {
+//	pascal[0][0] = 1;
+//	for (int i = 1; i <= 1000; i++) {
+//		pascal[i][0] = 1;
+//		for (int j = 1; j <= i; j++) {
+//			pascal[i][j] = (pascal[i - 1][j - 1] + pascal[i - 1][j])%10007;
+//		}
+//	}
+//}
+//
 //int main() {
 //
 //	////cin >> n >> k;
@@ -45,8 +78,16 @@
 //	//	}
 //	//	cout << cnt<<'\n';
 //	//}
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//	cin >> n >> k;
+//	go();
+//	//cout << (pec(n) / pec(n - k) / pec(k)) % 10007;
+//	cout << pascal[n][k];
+//	//vector<int> b;
+//	//combi(-1, b);
 //
-//
+//	//cout << cnt % 10007;
 //
 //	return 0;
 //}
