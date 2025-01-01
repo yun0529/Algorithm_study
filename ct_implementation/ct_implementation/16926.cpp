11d17c932c02bfbba6b38b71e408069cd73434f1
yun0529 <0529yun@naver.com>
2025-01-01 20:56:55 +0900

    16926 #7

--- ct_implementation/ct_implementation/16926.cpp
+++ ct_implementation/ct_implementation/16926.cpp
@@ -18,7 +18,7 @@
 //	int w = m, h = n;
 //	for (int i = 0; i < n / 2; i++) {
 //		if (h <= 1 || w <= 1) {
-//			radi[i] = 0;
+//			radi[i] = 1;
 //			break;
 //		}
 //		radi[i] = 2 * h + 2 * w - 4;
@@ -26,31 +26,37 @@
 //	}
 //
 //	for (int i = 0; i < n / 2; i++) {
+//		if (radi[i] == 0) continue;
 //		int roll = r % radi[i]; // 결과적으로 몇번 회전할지
 //		int cnt = 0;
-//		while (cnt <= roll) {
+//		while (cnt < roll) {
 //			int x = i, y = i, dir = 0; // 회전은 왼쪽 상단부터
 //			int tmp = arr[y][x];
-//			for (int j = 0; j < radi[i]; j++) {
+//			while(dir < 4) {
 //
 //				int nx = x + dx[dir];
 //				int ny = y + dy[dir];
 //
-//				if (nx >= m - i || ny >= n - i || nx < 0 || ny < 0) {
-//					dir = (dir + 1) % 4;
+//				if (nx >= m - i || ny >= n - i || nx < i || ny < i) {
+//					dir++;
 //				}
-//				arr[y][x] = arr[ny][nx];
-//				y = ny;
-//				x = nx;
+//				else {
+//					arr[y][x] = arr[ny][nx];
+//					y = ny;
+//					x = nx;
+//					
+//				}
+//				
 //			}
 //			arr[i + 1][i] = tmp;
-//			for (int i = 0; i < n; i++) {
-//				for (int j = 0; j < m; j++) {
-//					cout << arr[i][j] << ' ';
-//				}
-//				cout << '\n';
-//			}
 //			cnt++;
+//			//cout << "\n------------------------------------\n";
+//			//for (int i = 0; i < n; i++) {
+//			//	for (int j = 0; j < m; j++) {
+//			//		cout << arr[i][j] << ' ';
+//			//	}
+//			//	cout << '\n';
+//			//}
 //		}
 //		
 //	}
