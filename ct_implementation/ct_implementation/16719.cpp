a3d31e96e6458f882026524184d76adf5381b063
yun0529 <0529yun@naver.com>
2025-01-04 19:28:43 +0900

    16719, 14719 #7

--- ct_implementation/ct_implementation/16719.cpp
+++ ct_implementation/ct_implementation/16719.cpp
@@ -1,9 +1,37 @@
 //#include <iostream>
+//#include <vector>
+//#include<tuple>
 //using namespace std;
 //
 //char c[104];
 //string str, ret = "";
 //int arr[27];
+//
+//pair<char, int> srr[101];
+//
+//void printCur() {
+//	for (int i = 0; i < str.length(); i++) {
+//		if (srr[i].second != 0) cout << srr[i].first;
+//	}
+//	cout << '\n';
+//
+//	return;
+//}
+//void go(int start, int end) {
+//	// 가장 작은 인덱스 찾기
+//	int idx = start;
+//	for (int i = start; i <= end; i++) {
+//		if (srr[i].first < srr[idx].first) idx = i;
+//	}
+//	srr[idx].second = 1;
+//	printCur();
+//	// 인덱스 뒤로 위치할 것들 먼저 -> 사전순 하기 위함.
+//	if (idx + 1 <= end) go(idx + 1, end);
+//	// 인덱스 앞에 위치할 것들
+//	if (start <= idx - 1) go(start, idx - 1);
+//
+//	return;
+//}
 //
 //int main() {
 //	ios_base::sync_with_stdio(false);
@@ -11,50 +39,10 @@
 //
 //	cin >> str;
 //	for (int i = 0; i < str.length(); i++) {
-//		arr[str[i] - 'A']++;
+//		srr[i] = make_pair( str[i], 0 );
 //	}
-//	for (int i = 0; i < str.length(); i++) {
-//		int flag = true;
-//		for (int j = 0; j < 26; j++) {
-//			if (arr[j] >= 1) {
-//				if (ret.length() >= 1) {
-//					int idx = str.find(j + 'A'); // 원래 문자에서 넣을 문자 위치 찾기
-//					int cur = str.find(ret[0]); // 만드는 문자열의 가장 앞 문자 위치 찾기
-//					// 넣을 문자가 첫 문자보다 앞에 들어가야 하지만 사전상 뒤인 경우
-//					if (idx < cur && j > ret[0] - 'A') continue;
-//					// idx로 잘라서 문자 넣기
-//					if (idx >= ret.length()) {
-//						ret += j + 'A';
-//					}
-//					else {
-//						string pre = ret.substr(0, idx);
-//						string nex = ret.substr(idx, ret.length());
-//						pre += j + 'A';
-//						ret = pre + nex;
-//					}
-//					arr[j]--;
-//					flag = false;
-//					break;
-//				}
-//				else { // 처음은 빠른거 바로 입력
-//					ret += j + 'A';
-//					arr[j]--;
-//					flag = false;
-//					break;
-//				}
-//			}
-//		}
-//		if (flag) {
-//			for (int j = 0; j < 26; j++) {
-//				if (arr[j] >= 1) {
-//					ret = (char)(j + 'A') + ret;
-//					arr[j]--;
-//					break;
-//				}
-//			}
-//			
-//		}
-//		cout << ret << '\n';
-//	}
+//	go(0, str.length() - 1);
+//
+//	
 //	return 0;
 //}
