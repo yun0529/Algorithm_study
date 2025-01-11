e577216f21aec9ed8e52d0550a4675b13b542b19
yun0529 <0529yun@naver.com>
2025-01-11 23:03:40 +0900

    20053

---
+++ ct_implementation/ct_implementation/20053.cpp
@@ -0,0 +1,23 @@
+#if 01
+#include<bits/stdc++.h>
+using namespace std;
+
+int t, n, x;
+int mx = -987654321, mn = 987654321;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		cin >> n;
+		mx = -987654321, mn = 987654321;
+		for (int j = 0; j < n; ++j) {
+			cin >> x;
+			mx = max(x, mx);
+			mn = min(x, mn);
+		}
+		cout << mn << " " << mx << '\n';
+	}
+	return 0;
+}
+#endif // 01
