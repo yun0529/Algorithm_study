4b19f4f64f64bdec288b868c29216a8609819be7
yun0529 <0529yun@naver.com>
2025-01-18 16:22:08 +0900

    12865 #7

--- lecture/ch11/ch11/12865.cpp
+++ lecture/ch11/ch11/12865.cpp
@@ -4,45 +4,44 @@
 #include <algorithm>
 using namespace std;
 typedef long long ll;
-ll n,k,w,v, dp[104][104];
-ll mx = 0;
-vector<pair<int, int>> vec;
-int comp(int a, int b) {
-	return a < b;
+
+int n, k, vrr[104], wrr[104];
+ll dp[104][100004];
+
+void go() {
+	for (int i = 1; i <= k; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			if (wrr[j] > i) { // 무게 초과
+				dp[j][i] = dp[j - 1][i];
+			}
+			else { // 담거나 안담거나
+				// j의 가치 + j의 무게를 뺀 나머지에서 가치의 dp(최대 값)
+				// 이전까지의 최대값
+				// 비교
+				dp[j][i] = max(dp[j - 1][i - wrr[j]] + vrr[j], dp[j - 1][i]);
+			}
+		}
+	}
+
 }
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n >> k;
 
-	for (int i = 0; i < n; ++i) {
-		cin >> w >> v;
-		vec.push_back({ v,w });
+	for (int i = 1; i <= n; ++i) {
+		cin >> wrr[i] >> vrr[i];
 	}
-	ll wrr[104] = { k };
-	sort(vec.rbegin(), vec.rend());
-	//sort(vrr, vrr + n, comp);
-	//cout << "----\n";
-	//for (int i = 0; i < n; ++i) {
-	//	cout << vec[i].first <<' '<< vec[i].second<<'\n';
-	//}
-
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			
-			dp[i][j] += vec[j].first;
-			wrr[j] -= vec[j].second;
-			if (vec[j].second > weight)continue;
-			sum += vec[j].first;
-			weight -= vec[j].second;
-			//cout << sum << ' ' << weight << '\n';
-			if (weight <= 0) break;
-		}
-		mx = max(mx, sum);
+	for (int i = 0; i <= n; ++i) {
+		dp[i][0] = 0;
 	}
-	cout << mx;
-
+	for (int i = 0; i <= k; ++i) {
+		dp[0][i] = 0;
+	}
+	go();
+	cout << dp[n][k];
 	return 0;
 }
 #endif // 01
