c9479664db4967db5d21c29ca1d95df183004e52
yun0529 <0529yun@naver.com>
2025-02-01 00:44:06 +0900

    11444 #7

--- baekjoon/class4/class4/11444.cpp
+++ baekjoon/class4/class4/11444.cpp
@@ -1,23 +1,34 @@
-#if 01
+#if 0
 #include <iostream>
+#include <map>
+#include <math.h>
 using namespace std;
 typedef long long ll;
 
 ll n, mod = 1000000007, ret, arr[3];
+map<ll, ll> mp;
+
+ll fibo(ll n) {
+	if (n <= 2) return mp[n];
+	if (mp[n] != 0)	return mp[n];
+
+	if (n % 2 == 0)
+		return mp[n] = ((fibo(n / 2) % mod) * ((fibo(n / 2 + 1) % mod) + (fibo(n / 2 - 1) % mod))) % mod;
+	else
+		return mp[n] = (((fibo((n + 1) / 2) % mod) * (fibo((n + 1) / 2) % mod))
+			+ ((fibo((n - 1) / 2) % mod) * (fibo((n - 1) / 2) % mod))) % mod;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);cout.tie(NULL);
 
 	cin >> n;
-	arr[0] = 0;
-	arr[1] = 1;
-	arr[2] = 1;
-	for (ll i = 3;i <= n;++i) {
-		arr[i % 3] = (arr[(i+1) % 3] + arr[(i+2) % 3]) % mod;
-		ret = arr[i % 3];
-	}
-	cout << ret;
+	mp[0] = 0;
+	mp[1] = 1;
+	mp[2] = 1;
+	
+	cout << fibo(n);
 	return 0;
 }
 #endif // 01
