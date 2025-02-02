500a1cf44e29f9b4a8051f1c4f44da275a359207
yun0529 <0529yun@naver.com>
2025-02-02 18:21:50 +0900

    9251 #7

---
+++ baekjoon/class4/class4/9251.cpp
@@ -0,0 +1,37 @@
+#if 01
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ret;
+int dp[1004][1004];
+string str1, str2;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> str1 >> str2;
+	int len1 = str1.length();
+	int len2 = str2.length();
+	for (int i = 1; i <= len1; i++) {
+		for (int j = 1; j <= len2; j++) {
+			if (str1[i - 1] == str2[j - 1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+				
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+	//for (int i = 0; i <= str1.length(); i++)
+	//{
+	//	for (int j = 0; j <= str2.length(); j++)
+	//		cout << dp[i][j] << " ";
+	//	cout << endl;
+	//}
+	cout << dp[len1][len2];
+	return 0;
+}
+#endif // 01
