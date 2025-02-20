04e521ec4cd12187622c79f15c9a2e1564e70af3
yun0529 <0529yun@naver.com>
2025-02-20 17:47:21 +0900

    20366 solving... #7

---
+++ test/test_prac/test_prac/20366.cpp
@@ -0,0 +1,15 @@
+#if 01
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+
+
+    return 0;
+}
+#endif
