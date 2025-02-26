8a16ff3644d81785ae3f15be6471f3b11e51938a
yun0529 <0529yun@naver.com>
2025-02-26 21:50:47 +0900

    10942 #7

--- baekjoon/class5/class5/10942.cpp
+++ baekjoon/class5/class5/10942.cpp
@@ -11,21 +11,32 @@ int main()
 	cin.tie(0);cout.tie(0);
 
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
 	}
-	for (int i = 0; i < n - 1; i++) {
+	for (int i = 1; i <= n; i++) {
 		dp[i][i] = 1; // 한자리
 		if (arr[i] == arr[i + 1]) dp[i][i + 1] = 1; // 두자리
 	}
-	
-	for (int i = 1; i < n-1; i++) {
-		for (int j = 0; (i+j) < n; j++) {
-			if (arr[j] == arr[i + j] && dp[j + 1][i + j - 1] == 1)
+
+	for (int i = n-1; i >= 1; i--) { // 세자리 이상
+		for (int j = i - 2; j <= n; j++) {
+			if (arr[i] == arr[j] && dp[i+1][j-1] == 1) {
+				dp[i][j] = 1;
+			}
 		}
 	}
+	
+	//cout << "\n--------------------\n";
+	//for (int i = 0; i <=n; i++) { 
+	//	for (int j = 0; j<=n; j++) {
+	//		cout << dp[i][j] << ' ';
+	//	}
+	//	cout << '\n';
+	//}
+	//cout << "\n--------------------\n";
 	cin >> m;
-	for (int i = 0; i < n - 1; i++) {
+	for (int i = 0; i < m; i++) {
 		cin >> s >> e;
 		cout << dp[s][e] << '\n';
 	}
