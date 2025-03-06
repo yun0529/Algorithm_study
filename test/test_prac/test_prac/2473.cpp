484b7df939a55aebea41b75c7970d02e731238d6
yun0529 <0529yun@naver.com>
2025-03-06 21:10:52 +0900

    2473 #7

--- test/test_prac/test_prac/2473.cpp
+++ test/test_prac/test_prac/2473.cpp
@@ -1,15 +1,14 @@
-#if 01
+#if 0
 #include <iostream>
 #include <algorithm>
 typedef long long ll;
 using namespace std;
 
-int N, arr[5004];
-ll ret = 4e9; // |0 - ret| 해서 절댓값이 작은 값을 저장
-int result[3];
+ll N, arr[5004];
+ll ret = 4e9; // |ret| 해서 절댓값이 작은 값을 저장
+ll result[3];
 // 투포인터
-int l = 0;
-int r = N-1;
+// ll r = N-1;
 
 void input_data() {
     cin >> N;
@@ -18,29 +17,28 @@ void input_data() {
     }
 }
 
-bool bin_search(ll mid) {
-    ll sum = arr[l] + arr[mid] + arr[r];
-    if (abs(sum) < abs(ret)) {
-        ret = sum;
-        return true;
-    }
-    else return false;
-}
+//bool bin_search(ll mid) {
+//    ll sum = arr[l] + arr[mid] + arr[r];
+//    if (abs(sum) < abs(ret)) {
+//        ret = sum;
+//        return true;
+//    }
+//    else return false;
+//}
 
 void solution() {
     sort(arr, arr + N);
    
     for (int i = 0;i < N-2;i++) {
-        l = i;
-        r = N - 1;
-        int mid = l+1;
+        ll r = N - 1;
+        ll mid = i+1;
         while (mid < r) {
             //bool flag = bin_search(mid);
             //cout << flag;
-            ll sum = arr[l] + arr[mid] + arr[r];
+            ll sum = arr[i] + arr[mid] + arr[r];
             if (abs(sum) < ret) {
                 ret = abs(sum);
-                result[0] = arr[l];
+                result[0] = arr[i];
                 result[1] = arr[mid];
                 result[2] = arr[r];
                 if (ret == 0) return;
