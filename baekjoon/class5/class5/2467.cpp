e9e314cc4fac1b86013a5f1cdeb3eba18b564c89
yun0529 <0529yun@naver.com>
2025-04-01 00:44:52 +0900

    2467, 14003 #7

---
+++ baekjoon/class5/class5/2467.cpp
@@ -0,0 +1,42 @@
+#if 01
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, arr[100004], mn = 2e9;
+int ret[2];
+
+void solution() {
+	int l = 0;
+	int r = n - 1;
+	while(l < r) {
+		int tmp = arr[l] + arr[r];
+		//cout << tmp << ' ';
+		if (abs(tmp) < mn) {
+			ret[0] = arr[l];
+			ret[1] = arr[r];
+			mn = abs(tmp);
+		}
+
+		if (tmp > 0) r--;
+		else l++;
+	}
+	cout << ret[0] << " " << ret[1];
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	solution();
+
+	return 0;
+}
+#endif // 01
